644845453628446cc845dc090eff465857344a23
Aleksandr Shcherba <scherba.alexander@gmail.com>
2022-12-05 16:36:21 +0200

    remove customer errors and add product error

--- utils/exceptions.py
+++ utils/exceptions.py
@@ -1,12 +1,12 @@
-class CustomerValidationError(AssertionError):
-    """Errors for customer data"""
+class PurchaseValidationError(AssertionError):
+    """Errors for purchase logic"""
 
     def __init__(self, message):
         super().__init__(message)
 
 
-class PurchaseValidationError(AssertionError):
-    """Errors for purchase logic"""
+class ProductValidationError(AssertionError):
+    """Errors for product data"""
 
     def __init__(self, message):
         super().__init__(message)
